0376efda8a658ec2bbe2531a21f7102f89919278
nicholashaynes1 <nicholashaynes1@gmail.com>
2015-10-07 13:55:56 -0600

    The project now shows a popup.

--- src/list/controller/ArrayListController.java
+++ src/list/controller/ArrayListController.java
@@ -1,19 +1,40 @@
 package list.controller;
 
+import list.view.ListViewer;
+
 import java.util.ArrayList;
 
 public class ArrayListController
 {
 	private ArrayList<String>firstWords;
 	
+	/**
+	 * creates an instance of a popup
+	 */
+	private ListViewer popupDisplay;
+	
 	public ArrayListController()
 	{
 		firstWords = new ArrayList<String>();
+		popupDisplay = new ListViewer();
 	}
+	
 	
 	
 	public void start()
 	{
 		firstWords.add("CEHRHEIUFHEIUFHIHhefuihfwiuhiuawehrlhailuehiuhfwiahf");
+		firstWords.add("bababaabbabababbababbababbababababababbaba");
+		displayList();
 	}
+
+	private void displayList()
+	{
+		for(int spot = 0; spot < firstWords.size(); spot++)
+		{
+			popupDisplay.showList("The contents at " +spot+ " are " + firstWords.get(spot));
+		}
+	}
+
+
 }
